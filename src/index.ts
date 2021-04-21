e0f7630a4264e4fb977497e80351d6178c0f19d8
Nicolas <nicolas.marqui@fatec.sp.gov.br>
2021-04-21 00:45:02 -0300

    dev: Quem confia digita 1/2

--- src/index.ts
+++ src/index.ts
@@ -53,14 +53,14 @@ const main = async () => {
         host: process.env.POSTGRES_HOST,
         port: 5432,
         username: process.env.POSTGRES_USER,
-        // database: process.env.POSTGRES_DB,
+        database: process.env.POSTGRES_DB,
         password: process.env.POSTGRES_PASSWORD,
-        // ssl: process.env.NODE_ENV?.includes("production"),
-        // extra: {
-        //     ssl: {
-        //         rejectUnauthorized: false,
-        //     },
-        // },
+        ssl: process.env.NODE_ENV?.includes("production"),
+        extra: {
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        },
         // logging: true,
         migrations: [path.join(__dirname, "./migrations/*")],
         entities: [
